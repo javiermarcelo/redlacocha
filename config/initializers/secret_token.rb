fb56b96fe3435a56d32f51dc7d59b9ec88689e76
conexiononline <administrador@conexiononline.es>
2012-09-30 00:40:46 +0200

    red la cocha

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redlacocha::Application.config.secret_token = '03b902a6413acd8cd47dffdad53e26f6b71ab53c46f3e46e822abeef84a123f8622d56e1095fb9fbf33f203e144f14368fa577e48c9b9375e1357113501daf6b'
